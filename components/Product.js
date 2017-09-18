44f02390e007aa7d4be57cb203cd9e2bf24df6e1
Hannah Hudson <hannah.hch@gmail.com>
2017-09-18 13:37:29 -0400

    able to render product titles to the page, and added cors chrom add on to render

--- components/Product.js
+++ components/Product.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Product = ({ results }) => (
+const Product = ({ item }) => (
   <div className = "results">
-    <p>Product Name: {results.title}</p>
+    <p>Product Name: {item.title}</p>
   </div>
 );
 
